96614f38d6a6382002049e98971f7692c3f4cfb6
mahmoud youssef <49236858+mahmoudjoe3@users.noreply.github.com>
2021-01-08 20:05:49 +0200

    update to Loader
    AsyncTask is deprecated and is memory leak process so we can replace it by Loader

---
+++ app/src/main/java/com/example/android/quakereport/EarthQuakeLoader.java
@@ -0,0 +1,61 @@
+package com.example.android.quakereport;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.loader.content.AsyncTaskLoader;
+
+import com.example.android.quakereport.Model.item;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class EarthQuakeLoader extends AsyncTaskLoader<List<item>> {
+    private static final String TAG ="EarthquakeActivity" ;
+    String url;
+    public EarthQuakeLoader(@NonNull Context context, String url) {
+        super(context);
+        this.url=url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        Log.d(TAG, "onStartLoading: ");
+        forceLoad();
+    }
+
+    @Nullable
+    @Override
+    public List<item> loadInBackground() {
+
+        Log.d(TAG, "loadInBackground: ");
+        String JSONString = null;
+        try {
+            JSONString = HttpHandler.makeHttpRequest(create(url));
+        } catch (IOException e) {
+            Log.e(TAG, "onCreate: makeHttpRequest error->", e);
+        }
+        return QueryUtils.extractEarthquakes(JSONString);
+    }
+
+    private URL create(String urlStr) {
+        URL url = null;
+        try {
+            url = new URL(urlStr);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+}
--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -18,11 +18,14 @@ package com.example.android.quakereport;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -31,180 +34,116 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 import com.example.android.quakereport.Model.item;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
-import io.reactivex.Scheduler;
-import io.reactivex.Single;
-import io.reactivex.SingleObserver;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-
-public class EarthquakeActivity extends AppCompatActivity {
-    FloatingActionButton actionButton;
-    int MinMag=1;
-    int sYear=2019;
-    int eYear=2020;
-    int limit=20000;
-    String orderBy="time";
-    String urlStr ;
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<item>> {
+    private static final int EARTHQUAKE_LOADER_ID = 1;
+    int MinMag = 1;
+    int limit = 2000;
+    String orderBy = "time";
+    String urlStr;
     item_adapter adapter;
     ProgressBar progress;
-    private String getURL() {
-        
-        return "https://earthquake.usgs.gov/fdsnws/event/1/"+
-        "query?format=geojson&"+
-        /*"starttime=" +sYear+"-01-01&"+
-        "endtime="+eYear+"-01-01&"+
-        "minmagnitude="+MinMag+"&"+*/
-        "limit="+limit+"&"
-        +"orderby="+orderBy;
+    FloatingActionButton actionButton;
+    EarthQuakeLoader loader;
+    public static final String TAG = "EarthquakeActivity.me";
 
-    }
-
-    public static final String TAG = EarthquakeActivity.class.getName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        progress=findViewById(R.id.progress);
+        progress = findViewById(R.id.progress);
         RecyclerView earthquakeListView = findViewById(R.id.list);
-        adapter = new item_adapter( EarthquakeActivity.this);
+        actionButton = findViewById(R.id.filterBtn);
+
+        adapter = new item_adapter(EarthquakeActivity.this);
+        urlStr = getURL();
+
         earthquakeListView.setAdapter(adapter);
         earthquakeListView.setLayoutManager(new LinearLayoutManager(EarthquakeActivity.this));
 
-        urlStr = getURL();
-        new EarthQuakeAsyncTask().execute();
+        LoaderManager.getInstance(this).initLoader(EARTHQUAKE_LOADER_ID, null, this);
 
-        /*
-        getList().subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new SingleObserver<ArrayList<item>>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-                        progress.setVisibility(View.VISIBLE);
-
-                    }
-
-                    @Override
-                    public void onSuccess(ArrayList<item> items) {
-                        progress.setVisibility(View.GONE);
-                        adapter.setList(items);
-                        adapter.setOnClickListener(new item_adapter.OnClickListener() {
-                            @Override
-                            public void onClick(String url) {
-                                Intent intent = new Intent(Intent.ACTION_VIEW);
-                                intent.setData(Uri.parse(url));
-                                startActivity(intent);
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-                });
-*/
-        actionButton=findViewById(R.id.filterBtn);
         actionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final View view= getLayoutInflater().inflate(R.layout.filterdialog,null);
-                final EditText sY,eY,minMag;
-                sY=view.findViewById(R.id.startYear);
-                sY.setText(sYear+"");
-                eY=view.findViewById(R.id.endYear);
-                eY.setText(eYear+"");
-                minMag=view.findViewById(R.id.minMag);
-                minMag.setText(MinMag+"");
-                final Spinner order=view.findViewById(R.id.order);
-
-                AlertDialog.Builder builder=new AlertDialog.Builder(EarthquakeActivity.this);
-                builder.setView(view).setPositiveButton("Filter", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        MinMag=Integer.parseInt(minMag.getText().toString());
-                        sYear=Integer.parseInt(sY.getText().toString());
-
-                        eYear=Integer.parseInt(eY.getText().toString());;
-                        orderBy=order.getSelectedItem().toString();
-                        urlStr=getURL();
-
-                        new EarthQuakeAsyncTask().execute();
-                    }
-                }).setNegativeButton("Back",null).create().show();
+                createDialog().show();
             }
         });
     }
 
-    private URL create(String urlStr) {
-        URL url = null;
-        try {
-            url = new URL(urlStr);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        return url;
+
+    @NonNull
+    @Override
+    public Loader<List<item>> onCreateLoader(int id, @Nullable Bundle args) {
+        Log.d(TAG, "onCreateLoader: ");
+        loader = new EarthQuakeLoader(EarthquakeActivity.this, urlStr);
+        return loader;
     }
 
-
-    Single<ArrayList<item>> getList(){
-        String JSONString = null;
-        try {
-            JSONString = HttpHandler.makeHttpRequest(create(urlStr));
-        } catch (IOException e) {
-            Log.e(TAG, "onCreate: makeHttpRequest error->", e);
-        }
-        return Single.just(QueryUtils.extractEarthquakes(JSONString));
-    }
-
-    private class EarthQuakeAsyncTask extends AsyncTask<Void, Void, Void> {
-        List<item> earthquakes2=new ArrayList<>();
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progress.setVisibility(View.VISIBLE);
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            String JSONString = null;
-            try {
-                JSONString = HttpHandler.makeHttpRequest(create(urlStr));
-            } catch (IOException e) {
-                Log.e(TAG, "onCreate: makeHttpRequest error->", e);
+    @Override
+    public void onLoadFinished(@NonNull Loader<List<item>> loader, List<item> data) {
+        Log.d(TAG, "onLoadFinished: ");
+        progress.setVisibility(View.GONE);
+        adapter.setList(data);
+        adapter.setOnClickListener(new item_adapter.OnClickListener() {
+            @Override
+            public void onClick(String url) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
             }
-
-            earthquakes2 = QueryUtils.extractEarthquakes(JSONString);
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            progress.setVisibility(View.GONE);
-            adapter.setList(earthquakes2);
-            adapter.setOnClickListener(new item_adapter.OnClickListener() {
-                @Override
-                public void onClick(String url) {
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(url));
-                    startActivity(intent);
-                }
-            });
-        }
+        });
     }
 
+    @Override
+    public void onLoaderReset(@NonNull Loader<List<item>> loader) {
+        Log.d(TAG, "onLoaderReset: ");
+    }
+
+    private String getURL() {
+        return "https://earthquake.usgs.gov/fdsnws/event/1/" +
+                "query?format=geojson&" +
+                "minmagnitude=" + MinMag + "&" +
+                "limit=" + limit + "&"
+                + "orderby=" + orderBy;
+    }
+
+    private AlertDialog createDialog() {
+
+        final View view = getLayoutInflater().inflate(R.layout.filterdialog, null);
+        final EditText minMag,rows;
+
+        minMag = view.findViewById(R.id.minMag);
+        minMag.setText(MinMag + "");
+        rows = view.findViewById(R.id.rows);
+        rows.setText(limit + "");
+        final Spinner order = view.findViewById(R.id.order);
+
+        return new AlertDialog.Builder(EarthquakeActivity.this)
+                .setView(view).setPositiveButton("Filter", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MinMag = Integer.parseInt(minMag.getText().toString());
+                        limit = Integer.parseInt(rows.getText().toString());
+
+                        orderBy = order.getSelectedItem().toString();
+                        urlStr = getURL();
+
+                        progress.setVisibility(View.VISIBLE);
+
+                        loader.setUrl(urlStr);
+                        loader.onStartLoading();
+
+                    }
+                }).setNegativeButton("Back", null).create();
+
+    }
+
+    
 }
